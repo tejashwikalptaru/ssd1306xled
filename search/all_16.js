ce9e5bb72a83ef87be6afc30cc8a5ecbc2ca4d86
tejashwikalptaru <tejashwikalptaru@users.noreply.github.com>
2026-03-31 19:34:22 +0000

    deploy: 5cd5f690ec3b5fe4db8c64b8737c9e4914e3a40b

---
+++ search/all_16.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['v0_20x_20to_20v1_200_0',['Migrating from v0.x to v1.0',['../migration.html',1,'']]],
+  ['v1_200_1',['Migrating from v0.x to v1.0',['../migration.html',1,'']]],
+  ['variable_20in_20ssd1306_5fstring_5ff8x16_2',['Uninitialized variable in ssd1306_string_f8x16',['../migration.html#uninit_var',1,'']]],
+  ['version_20number_3',['Version number',['../migration.html#version_number',1,'']]],
+  ['vertical_20addressing_4',['Vertical addressing',['../architecture.html#vertical_addressing',1,'']]]
+];
